3141f138af351658b0a3f25486b723a5b73128fc
Steven Robertson <stever@hey.com>
2022-12-27 19:39:38 +0000

    Changed the Codespace env var handling in constants

--- apps/web/src/constants.js
+++ apps/web/src/constants.js
@@ -2,18 +2,12 @@
 const isDev = DEBUG || false;
 const devLogging = false;
 
-console.log('CODESPACE_NAME', CODESPACE_NAME);
-console.log('GITHUB_CODESPACES_PORT_FORWARDING_DOMAIN', GITHUB_CODESPACES_PORT_FORWARDING_DOMAIN);
-
 let hostname;
 let protocol;
 if (!isDev) {
     hostname = "domaindriven.dev";
     protocol = "https";
-} else if (CODESPACE_NAME && 
-           CODESPACE_NAME !== 'undefined' && 
-           GITHUB_CODESPACES_PORT_FORWARDING_DOMAIN && 
-           GITHUB_CODESPACES_PORT_FORWARDING_DOMAIN !== 'undefined') {
+} else if (CODESPACE_NAME && GITHUB_CODESPACES_PORT_FORWARDING_DOMAIN) {
     hostname = `${CODESPACE_NAME}-8080.${GITHUB_CODESPACES_PORT_FORWARDING_DOMAIN}`;
     protocol = "https";
 } else {
